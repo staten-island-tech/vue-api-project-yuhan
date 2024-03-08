1cf28f1bd8f76bd48d0eaef78da8df2364cf8ba5
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-03-08 08:48:36 -0500

    sdfsdf

--- vue-api/src/views/AboutView.vue
+++ vue-api/src/views/AboutView.vue
@@ -4,6 +4,21 @@
   </div>
 </template>
 
+<script setup>
+
+import { ref, onBeforeMount, onMounted } from 'vue';
+const pokemon = ref("");
+async function getPokemon(){
+  let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+  let data = await res.json();
+  pokemon.value = data.results;
+
+}
+
+onMounted(() => {
+  getPokemon()
+})
+</script>
 <style>
 @media (min-width: 1024px) {
   .about {
