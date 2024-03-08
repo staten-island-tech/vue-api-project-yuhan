90555c27f2270fed6944f917829efe42f7a5f867
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-03-08 09:19:34 -0500

    starting chart

--- vue-api/src/views/AboutView.vue
+++ vue-api/src/views/AboutView.vue
@@ -1,23 +1,53 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Chart</h1>
   </div>
+  <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
 </template>
 
 <script setup>
 
 import { ref, onBeforeMount, onMounted } from 'vue';
-const pokemon = ref("");
-async function getPokemon(){
+
+const abc = ref("");
+async function getData(){
   let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
   let data = await res.json();
-  pokemon.value = data.results;
-
+  abc.value = data.results;
+  console.log(data)
 }
 
 onMounted(() => {
-  getPokemon()
+  getData()
 })
+
+(async function() {
+  const data = [ 
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+new Chart(
+  document.getElementById('acquisitions'),
+  {
+    type: 'bar',
+    data: {
+      labels: data.map(row => row.year),
+      datasets: [
+        {
+          label: 'Acquisitions by year',
+          data: data.map(row => row.count)
+        }
+      ]
+    }
+  }
+)})
+
 </script>
 <style>
 @media (min-width: 1024px) {
