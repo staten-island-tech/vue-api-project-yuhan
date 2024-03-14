f452a6589cdd1516859ae34bc509cb3a1e33222a
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-03-14 09:22:33 -0400

    class does not have id object object

--- vue-api/src/components/BarChart.vue
+++ vue-api/src/components/BarChart.vue
@@ -10,7 +10,7 @@
 import { Bar,} from "vue-chartjs";
 import { Chart as ChartJS, Title, Tooltip, Legend, CategoryScale, LinearScale, } from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, CategoryScale, LinearScale)
+ChartJS.register(Bar, Title, Tooltip, Legend, CategoryScale, LinearScale)
 
 
 const BarChart = {
