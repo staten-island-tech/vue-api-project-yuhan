c8ddfa339795e13b207167cacd5f9cf4eda26ea9
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-03-21 09:18:47 -0400

    works

--- vue-api/src/views/AboutView.vue
+++ vue-api/src/views/AboutView.vue
@@ -1,21 +1,4 @@
-<!-- <template>
- <BarChart></BarChart> Chart couldn't be loaded.
-</template>
 
-<script>
-import BarChart from '../components/BarChart.vue';
-
-
- export default {
-
-  components: { BarChart }
-} 
-
-</script>
-<style>
-
-</style>
- -->
 <template>
   Hello
   <BarChart/>
