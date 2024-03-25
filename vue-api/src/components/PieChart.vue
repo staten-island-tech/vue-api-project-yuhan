4667c25c6d76947cc9551d75946f675191ed2eef
eriks61 <144455584+eriks61@users.noreply.github.com>
2024-03-25 09:20:32 -0400

    start pie chart setup

---
+++ vue-api/src/components/PieChart.vue
@@ -0,0 +1,34 @@
+<script>
+import { Pie} from "vue-chartjs";
+import { Chart as ChartJS, Tooltip, Legend, ArcElement,} from 'chart.js'
+
+
+ChartJS.register(Tooltip, Legend, ArcElement,)
+
+export default  {
+    name: 'PieChart',
+    components: { Pie }, 
+    props: {
+  
+  },
+    data(){
+        return{
+            chartData: {
+                labels: this.labels,
+                datasets: [ 
+                    {
+                        label: 'Number Of Deaths',
+                        backgroundColor: '#ADD8E6',
+                        data: this.data
+                    }
+                ]
+            },
+            chartOptions: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        }
+    }
+}
+
+</script>
--- vue-api/src/views/HomeView.vue
+++ vue-api/src/views/HomeView.vue
@@ -1,26 +1,53 @@
 <script setup>
 import { ref, onMounted } from 'vue';
+import Chart from '@/components/PieChart.vue';
 
-const abc = ref("");
-async function getData(){
+const abc = [];//data from api
+let years = []; //Array all year values
+const loaded = ref(false); //loaded or not (to wait for api values)
+
+async function getData(year){
   try {
-    let res = await fetch("https://data.cityofnewyork.us/resource/jb7j-dtam.json");
+    let res = await fetch(`
+    https://data.cityofnewyork.us/resource/jb7j-dtam.json?$query=SELECT%0A%20%20%60year%60%2C%0A%20%20%60leading_cause%60%2C%0A%20%20%60sex%60%2C%0A%20%20%60race_ethnicity%60%2C%0A%20%20%60deaths%60%2C%0A%20%20%60death_rate%60%2C%0A%20%20%60age_adjusted_death_rate%60%0AWHERE%20caseless_one_of(%60year%60%2C%20%22${year}%22)`);
     let data = await res.json();
-    // let deds = await res.leading_cause
-    // console.log(deds)
-    console.log(data)
+    return data
   } catch (e) {
     console.error(e);
   }
 }
-  onMounted(() => {
-  getData()
-})
 
-
+async function chart(){
+for(let i = 0; i < years.length; i++){
+  let yearfiltered = await getData(years[i])
+  console.log(yearfiltered);
+  let deathtotal = 0;
+   for(let i = 0; i < yearfiltered.length; i++) {
+  deathtotal += Number(yearfiltered[i].year) //years was a string;
+}
+console.log(deathtotal);
+abc.push(deathtotal);
+}
+console.log(abc);
+loaded.value = true;
+}
+  onMounted(async() => {
+    loaded.value = false;
+    const fetchYears = await fetch( //To get all the years
+    "https://data.cityofnewyork.us/resource/jb7j-dtam.json?$query=SELECT%20%60year%60%20GROUP%20BY%20%60year%60%20ORDER%20BY%20%60year%60%20DESC%20NULL%20FIRST"
+  );
+  years = await fetchYears.json();
+  years = years.map((yr) => yr.year);
+  years = years.reverse();
+  console.log(years);
+  chart(); 
+});
 
 </script>
 
 <template>
-
+  <div class="big">
+ <Chart v-if ="loaded" :data="abc" :labels="years"/> 
+      <h1 v-else>wait ........</h1>
+    </div>
   </template>
